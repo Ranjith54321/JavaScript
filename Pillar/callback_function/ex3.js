6bce459228080ee07f3596d7377b9f3de93fe9da
Ranjith S <39690967+Ranjith54321@users.noreply.github.com>
2021-05-19 23:16:12 +0530

    Update ex3.js

--- Pillar/callback_function/ex3.js
+++ Pillar/callback_function/ex3.js
@@ -1,5 +1,5 @@
 // Evenet Listeners 
 
-document.getElementById("clickme").addEventListener("click",function abc(){ // here name less function is also fine but inorder to find inside the cintainor we  use named funtion
+document.getElementById("clickme").addEventListener("click",function abc(){ // here name less function is also fine but inorder to find inside the container we  use named funtion
     console.log("I'm clicked");
 })
